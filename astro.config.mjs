d97fa01ae698072633834e5c411548d8e8ef0c37
Braden Wong <bmw02002turbo@gmail.com>
2022-09-26 16:55:06 -0400

    Formatting: Tabs

--- astro.config.mjs
+++ astro.config.mjs
@@ -14,32 +14,32 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // https://astro.build/config
 export default defineConfig({
-	// Astro uses this full URL to generate your sitemap and canonical URLs in your final build
-	site: SITE.origin,
-	base: SITE.basePathname,
+  // Astro uses this full URL to generate your sitemap and canonical URLs in your final build
+  site: SITE.origin,
+  base: SITE.basePathname,
 
-	output: 'static',
+  output: 'static',
 
-	integrations: [
-		tailwind({
-			config: {
-				applyBaseStyles: false,
-			},
-		}),
-		sitemap(),
-		image(),
+  integrations: [
+    tailwind({
+      config: {
+        applyBaseStyles: false,
+      },
+    }),
+    sitemap(),
+    image(),
 
-		/* Disable this integration if you don't use Google Analytics (or other external script). */
-		partytown({
-			config: { forward: ['dataLayer.push'] },
-		}),
-	],
+    /* Disable this integration if you don't use Google Analytics (or other external script). */
+    partytown({
+      config: { forward: ['dataLayer.push'] },
+    }),
+  ],
 
-	vite: {
-		resolve: {
-			alias: {
-				'~': path.resolve(__dirname, './src'),
-			},
-		},
-	},
+  vite: {
+    resolve: {
+      alias: {
+        '~': path.resolve(__dirname, './src'),
+      },
+    },
+  },
 });
