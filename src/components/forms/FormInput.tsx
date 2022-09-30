45b147a155c19815e96f6f7433a3e9b4819c73b5
Braden Wong <bmw02002turbo@gmail.com>
2022-09-29 22:56:21 -0400

    Feat: Input

---
+++ src/components/forms/FormInput.tsx
@@ -0,0 +1,24 @@
+/* This example requires Tailwind CSS v2.0+ */
+import { useState } from 'react';
+
+export default function Example() {
+  const [input, setInput] = useState('')
+  return (
+    <div>
+      <label htmlFor="name" className="block text-sm font-medium text-gray-700">
+        Name
+      </label>
+      <div className="mt-1 border-b border-gray-300 focus-within:border-indigo-600">
+        <input
+          type="text"
+          name="name"
+          id="name"
+          className="block w-full border-0 border-b border-transparent bg-gray-50 focus:border-indigo-600 focus:ring-0 sm:text-sm"
+          placeholder="Jane Smith"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}kk
+        />
+      </div>
+    </div>
+  );
+}
