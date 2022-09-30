e0b472b300f49e3fff9c7320f17f2222af743d49
Braden Wong <bmw02002turbo@gmail.com>
2022-09-29 22:59:45 -0400

    Feat: Rose

--- src/components/forms/FormInput.tsx
+++ src/components/forms/FormInput.tsx
@@ -8,12 +8,12 @@ export default function Example() {
       <label htmlFor="name" className="block text-sm font-medium text-gray-700">
         Name
       </label>
-      <div className="mt-1 border-b border-gray-300 focus-within:border-indigo-600">
+      <div className="mt-1 border-b border-gray-300 focus-within:border-rose-600">
         <input
           type="text"
           name="name"
           id="name"
-          className="block w-full border-0 border-b border-transparent bg-gray-50 focus:border-indigo-600 focus:ring-0 sm:text-sm"
+          className="block w-full border-0 border-b border-transparent bg-gray-50 focus:border-rose-600 focus:ring-0 sm:text-sm"
           placeholder="Jane Smith"
           value={input}
           onChange={(e) => setInput(e.target.value)}kk
