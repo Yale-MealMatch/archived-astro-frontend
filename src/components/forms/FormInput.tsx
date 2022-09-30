bfeacf50745feba463bd85107f7bf6b435085adf
Braden Wong <bmw02002turbo@gmail.com>
2022-09-29 23:02:02 -0400

    Feat: Props

--- src/components/forms/FormInput.tsx
+++ src/components/forms/FormInput.tsx
@@ -1,20 +1,20 @@
 /* This example requires Tailwind CSS v2.0+ */
 import { useState } from 'react';
 
-export default function Example() {
+export default function Example({label, id, placeholder}) {
   const [input, setInput] = useState('')
   return (
     <div>
       <label htmlFor="name" className="block text-sm font-medium text-gray-700">
-        Name
+      {label}
       </label>
       <div className="mt-1 border-b border-gray-300 focus-within:border-rose-600">
         <input
           type="text"
-          name="name"
-          id="name"
+          name={id}
+          id={id}
           className="block w-full border-0 border-b border-transparent bg-gray-50 focus:border-rose-600 focus:ring-0 sm:text-sm"
-          placeholder="Jane Smith"
+          placeholder={placeholder}
           value={input}
           onChange={(e) => setInput(e.target.value)}kk
         />
